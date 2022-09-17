cc8e8a3cb9ec321784b5a84e97a0fb5bc1435e82
Firstname Lastname <hera.lindsay@gmail.com>
2022-09-17 12:47:51 +1200

    playsite

---
+++ playsite/main.js
@@ -0,0 +1,46 @@
+//let fa = ['']
+//for (let i = 0; i < 5; i++) {
+//  console.log('FA', i)
+//}
+
+///// experiment time /////
+
+//const faFA = ['FA', 'BA', 'LA', 'FA', 'FA', 'FA', 'FA', 'FA', 'MA']
+
+//const runRun = ['RUN', 'RUN', 'RUN', 'RUN', 'RUN', 'RUN']
+
+//let arrLength = faFA.length
+
+//function logNames(faFA) {
+//  for (let i = 0; i < 4; i++) console.log(faFA[i])
+//}
+
+//function printResult(faFa) {
+// console.log('fruit: ' + faFA)
+//}
+
+const faFA = ['FA', 'FA', 'FA', 'FA', 'FA', 'FA', 'FA', 'FA', 'FA']
+
+for (let i = 0; i < faFA.length; i++) {
+  console.log(faFA[i])
+}
+
+const runRun = ['RUN', 'RUN', 'RUN', 'RUN', 'RUN', 'RUN']
+
+for (let i = 0; i < runRun.length; i++) {
+  console.log(runRun[i])
+}
+
+function psychoKiller(faFa, runRun) {}
+
+// psychoKiller()
+///
+
+//  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+//  <script>
+//    let typed = new Typed(".auto-type",  {
+//  strings: ["PSYCHO ^700  KILLER" , "QU'EST-CE QUE C'EST?" , "FA" , "FA ^1000 BETTER" ,"RUN" , "RUN ^1000 AWAY", "^1000 OH OH OH"],
+//  typeSPeed: 50,
+//  loop: true,
+//})
